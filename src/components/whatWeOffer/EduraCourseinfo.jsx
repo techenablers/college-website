ed3d46a2564429939ea61a10e341f0f43b1928f3
muralidhara_b <muralidhara.b@hidini.io>
2025-02-28 14:45:59 +0530

    styling

--- src/components/whatWeOffer/EduraCourseinfo.jsx
+++ src/components/whatWeOffer/EduraCourseinfo.jsx
@@ -2,24 +2,46 @@
 import React from "react";
 import arrow from "../../../src/assets/Vector.svg";
 import WhatWeOfferUi from "./WhatWeOfferUi";
+import "../../../src/styles/OurOfferContent.css";
 
 function EduraCourseinfo() {
   return (
-    <div className="d-flex justify-content-center align-items-center">
-      <span style={{ textAlign: "center" }}>
+    <div className="d-flex ">
+      <span
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
         <WhatWeOfferUi />
+        <h5>Choose Any Courses</h5>
+        <p>
+          Standards in leadership skills synergize optimal <br /> expertise
+          rather than innovative leadership <br /> skills and better learning.{" "}
+        </p>
       </span>
       {/* <ScoopeContent/> */}
       <img src={arrow} alt="My SVG Icon" width={150} height={100} />
       <span>
         <WhatWeOfferUi />
+        <h5>Choose Any Courses</h5>
+        <p>
+          Standards in leadership skills synergize optimal <br /> expertise
+          rather than innovative leadership <br /> skills and better learning.{" "}
+        </p>
       </span>
+
       <img src={arrow} alt="My SVG Icon" width={150} height={100} />
       <span>
         <WhatWeOfferUi />
+        <h5>Choose Any Courses</h5>
+        <p>
+          Standards in leadership skills synergize optimal <br /> expertise
+          rather than innovative leadership <br /> skills and better learning.{" "}
+        </p>
       </span>
-
-      <p></p>
     </div>
   );
 }
