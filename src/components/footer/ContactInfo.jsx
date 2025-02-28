822811d56f1ddc11a073d15176b8488e0980f409
muralidhara_b <muralidhara.b@hidini.io>
2025-02-28 10:32:48 +0530

    ourfacility

---
+++ src/components/footer/ContactInfo.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function ContactInfo() {
+  return (
+    <div className="d-flex justify-content-around align-items-center" style={{padding:'0rem 10rem'}}>
+      <div
+        className="contact-item"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <strong>Call us any time:</strong>
+        <span style={{ marginTop: "5px" }}>+256 214 203 215</span>
+      </div>
+      <div
+        style={{
+          borderLeft: "2px solid #ffffff",
+          height: "30px",
+          margin: "0 10px",
+        }}
+      ></div>
+      <div
+        className="contact-item"
+        style={{
+          display: "flex",
+          flexDirection: "column", // Stack the content vertically
+          alignItems: "center",
+        }}
+      >
+        <strong>Email us 24/7 hours:</strong>
+        <span style={{ marginTop: "5px" }}>info@edura.com</span>
+      </div>
+      <div
+        style={{
+          borderLeft: "2px solid #ffffff",
+          height: "30px",
+          margin: "0 10px",
+        }}
+      ></div>
+      <div
+        className="contact-item"
+        style={{
+          display: "flex",
+          flexDirection: "column", // Stack the content vertically
+          alignItems: "center",
+        }}
+      >
+        <strong>Our university location:</strong>
+        <span style={{ marginTop: "5px" }}>ABCD location</span>
+      </div>
+    </div>
+  );
+}
+
+export default ContactInfo;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+import ContactInfo from "./ContactInfo";
+
+function Footer() {
+  return (
+    <div
+      className=" "
+      style={{
+        width: "100%",
+        backgroundColor: "#0B0E2A",
+      }}
+    >
+      <ContactInfo />
+    </div>
+  );
+}
+
+export default Footer;
