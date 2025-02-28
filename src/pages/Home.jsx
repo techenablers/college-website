fcb8dc3f2dda8872e1ad68823cdffd3dcb5e1100
Akshay AS <akshay.as222@gmail.com>
2025-02-28 12:50:52 +0530

    Added Offerings and About University

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import Offerings from "../components/Offerings/offerings";
+import AboutUniversity from "../components/AboutUniversity/AboutUniversity";
 
 const Home = () => {
   return <div>
   <Header />
   <Banner />
+  <Offerings />
+  <AboutUniversity />
 </div>
 };
 
