4413295705847258eb0692310b3f0313277db924
techenablers <techenablers2024@gmail.com>
2025-03-03 19:44:12 +0530

    Update ImportantDates.jsx

--- src/components/collegeInformation/ImportantDates.jsx
+++ src/components/collegeInformation/ImportantDates.jsx
@@ -33,7 +33,7 @@ function ImportantDates() {
     <div style={{width:'auto', maxWidth:'350PX', height:'400PX', backgroundColor:'#ffffff',overflowY:'auto', borderRadius: "5px", padding: "25px",scrollbarWidth: "thin"}} >
     <div className='d-flex justify-content-between align-items-center'>
         <h4>Important Dates</h4>
-        <p>see All</p>
+        <p>See All</p>
     </div>
     <hr/>
     {notice.map((val,indx)=>(
