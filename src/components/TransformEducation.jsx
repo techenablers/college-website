56817cade6acb67a8e3db2de0e33b3eb011c5b32
muralidhara_b <muralidhara.b@hidini.io>
2025-03-03 07:36:28 +0530

    kaddi part

--- src/components/TransformEducation.jsx
+++ src/components/TransformEducation.jsx
@@ -4,16 +4,7 @@ import "../styles/TransformEducation.css";
 function TransformEducation() {
   return (
     <div className="transform-education-content " style={{width:'100%', height:'500PX'}}>
-      {/* <p className="d-inline-block what-we-offer-header">
-        <span>
-          <i
-            className="fa fa-book"
-            aria-hidden="true"
-            style={{ fontSize: "16px" }}
-          ></i>{" "}
-          What We Offer
-        </span>
-      </p> */}
+   
     </div>
   );
 }
