9ba9740434cbd57c7c6d70d03f3297ee95695fe0
muralidhara_b <muralidhara.b@hidini.io>
2025-03-03 19:05:58 +0530

    banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
-import { Navbar, Nav, Container, Button, Form, InputGroup } from "react-bootstrap";
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaYoutube, FaSkype, FaSearch } from "react-icons/fa";
+// import {
+//   Navbar,
+//   Nav,
+//   Container,
+//   Button,
+//   Form,
+//   InputGroup,
+// } from "react-bootstrap";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaLinkedinIn,
+  FaYoutube,
+  FaSkype,
+  // FaSearch,
+} from "react-icons/fa";
 import "../styles/Header.css";
 import logo from "../assets/logo.svg";
 
@@ -8,7 +23,7 @@ const Header = () => {
   return (
     <>
       {/* Top Bar */}
-      <div className="top-bar d-flex justify-content-between align-items-center px-4 py-2">
+      {/* <div className="top-bar d-flex justify-content-between align-items-center px-4 py-2">
         <div className="contact-info">
           <span>256 214 203 215</span> | <span>info@edura.com</span>
         </div>
@@ -17,9 +32,9 @@ const Header = () => {
           <FaFacebookF /> <FaTwitter /> <FaLinkedinIn /> <FaYoutube /> <FaSkype />
         </div>
       </div>
-
+ */}
       {/* Main Navbar */}
-      <Navbar expand="lg" className="main-navbar py-3">
+      {/* <Navbar expand="lg" className="main-navbar py-3">
         <Container className="d-flex align-items-center justify-content-between">
           <Navbar.Brand href="#">
             <img src={logo} alt="DR.K.S. JR COLLEGE" className="logo" />
@@ -35,7 +50,6 @@ const Header = () => {
             </Nav>
           </Navbar.Collapse>
           <div className="d-flex align-items-center gap-3">
-            {/* Search Bar with Icon Inside */}
             <InputGroup className="search-bar">
               <Form.Control
                 type="search"
@@ -50,7 +64,77 @@ const Header = () => {
             <Button className="apply-now">APPLY NOW</Button>
           </div>
         </Container>
-      </Navbar>
+      </Navbar> */}
+
+      <div className="top-bar d-flex  px-4 py-2">
+        <div
+          style={{
+            width: "30%",
+            height: "35px",
+            backgroundColor: "#FA3A35",
+            position: "absolute",
+            right: "0",
+            top: "0",
+            clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%, 0 35px, 40px 0)", // Clip the top-left corner only
+          }}
+        >
+          <div className="social-icons d-flex align-items-end justify-content-center">
+            <span className="me-2">Follow Us</span>
+            <FaFacebookF /> <FaTwitter /> <FaLinkedinIn /> <FaYoutube />{" "}
+            <FaSkype />
+          </div>
+        </div>
+        <div
+          className=""
+          style={{
+            width: "90%",
+            height: "65px",
+            backgroundColor: "#ffffff",
+            position: "absolute",
+            right: "0",
+            bottom: "0",
+            borderTopLeftRadius: "60px",
+          }}
+        >
+          {/* <Navbar expand="lg" className=" py-3">
+            <Container className="d-flex align-items-center justify-content-between"></Container>
+          </Navbar> */}
+
+          <nav className="navbar" style={{ position: "relative", top: "5px" }}>
+            <div className="container d-flex justify-content-between">
+              <a className="navbar-brand" href="#" style={{ marginRight: "100px" }}>
+                <img src={logo} alt="DR.K.S. JR COLLEGE" className="logo" />
+              </a>
+
+              <div className="navbar-nav d-flex flex-row">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+                <a className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </a>
+
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Username"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                ></input>
+                <button className="apply-now" type="submit">
+                  Apply Now
+                </button>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
