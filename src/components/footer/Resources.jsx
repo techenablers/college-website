77f76bf9edea97aa344bf88af321f30d57165133
muralidhara_b <muralidhara.b@hidini.io>
2025-03-04 08:43:11 +0530

    footer changes

---
+++ src/components/footer/Resources.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import DashDesign from "./DashDesign";
+
+function Resources() {
+  return (
+    <div  style={{color:"#ffffff"}}>
+      {" "}
+      <h4> Resorce</h4>
+      <DashDesign />
+      <ul>
+        <li>Cumunity </li>
+        <li>Sports</li>
+        <li>Video Guides</li>
+        <li>Health Couch</li>
+        <li>Documentation </li>
+        <li>Security</li>
+        <li>Templet</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Resources;
