34bcd2e178cdaaeea939e191bf46be3a9dc491cc
muralidhara_b <muralidhara.b@hidini.io>
2025-03-04 21:12:38 +0530

    style

--- src/components/EducationSection/educationSection.jsx
+++ src/components/EducationSection/educationSection.jsx
@@ -1,5 +1,9 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import modren from "../../assets/modren.svg";
+import Placement from "../../assets/placement.svg";
+import expor from "../../assets/expor.svg";
 import "./EducationSection.css";
 
 const EducationSection = () => {
@@ -8,20 +12,24 @@ const EducationSection = () => {
       <Row className="justify-content-center">
         <Col lg={5} md={6} className="text-section">
           <h6 className="text-uppercase text-muted">Why Choose Us</h6>
-          <h1 className="SemiBold">Transform Education Your Life, Change the World</h1>
+          <h1 className="SemiBold">
+            Transform Education Your Life, Change the World
+          </h1>
           <Row className="features mt-4">
             <Col xs={6} className="mb-3">
               <Card className="feature-card">
                 <Card.Body>
-                  <i className="icon">🎓</i>
-                  <Card.Title className="SemiBold">Accreditation & Affiliations</Card.Title>
+                  <img src={Placement} />
+                  <Card.Title className="SemiBold">
+                    Accreditation & Affiliations
+                  </Card.Title>
                 </Card.Body>
               </Card>
             </Col>
             <Col xs={6} className="mb-3">
               <Card className="feature-card">
                 <Card.Body>
-                  <i className="icon">🏫</i>
+                  <img src={modren} />
                   <Card.Title>Modern Campus & Infrastructure</Card.Title>
                 </Card.Body>
               </Card>
@@ -29,7 +37,8 @@ const EducationSection = () => {
             <Col xs={6} className="mb-3">
               <Card className="feature-card">
                 <Card.Body>
-                  <i className="icon">👨‍🏫</i>
+                  {/* <i className="icon">👨‍🏫</i> */}
+                  <img src={expor} />
                   <Card.Title>Expert Faculty</Card.Title>
                 </Card.Body>
               </Card>
@@ -37,7 +46,7 @@ const EducationSection = () => {
             <Col xs={6} className="mb-3">
               <Card className="feature-card">
                 <Card.Body>
-                  <i className="icon">💼</i>
+                  <img src={Placement} />
                   <Card.Title>Placement Assistance</Card.Title>
                 </Card.Body>
               </Card>
@@ -46,8 +55,7 @@ const EducationSection = () => {
         </Col>
 
         <Col lg={5} md={6} className="image-placeholder">
-          <div className="dark-box">
-          </div>
+          <div className="dark-box"></div>
         </Col>
       </Row>
     </Container>
