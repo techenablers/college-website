20e932fa4b76f3578b4449550e882f63f0ac390a
Muralidharab <88184237+Muralidharab@users.noreply.github.com>
2025-03-07 18:31:07 +0530

    Update WhatWeOfferUi.jsx

--- src/components/whatWeOffer/WhatWeOfferUi.jsx
+++ src/components/whatWeOffer/WhatWeOfferUi.jsx
@@ -1,17 +1,31 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import '../../../src/styles/WhatWeOfferUi.css'
+import React from "react";
+import "../../../src/styles/WhatWeOfferUi.css";
+import vector from "../../assets/vector.svg";
 
-
-function WhatWeOfferUi() {
-    return (
-        <div className="container" style={{display:'flex', flexDirection:'column', textAlign:'center'}}>
-          <div className="scooped-blue-circle"></div>
-          <div className="scooped-square">
-            <div className="scooped-ball"></div>
-          </div>
-        </div>
-    );
+function WhatWeOfferUi({ img }) {
+  return (
+    <div
+      className="container"
+      style={{ display: "flex", flexDirection: "column", textAlign: "center" }}
+    >
+      <div
+        className="scooped-blue-circle"
+        style={{
+          backgroundImage: `url(${vector})`,
+        }}
+      ></div>
+      <div
+        className="scooped-square"
+        style={{
+          backgroundImage: `url(${img})`,
+        }}
+      >
+        <div className="scooped-ball"></div>
+      </div>
+    </div>
+  );
 }
 
-export default WhatWeOfferUi
+export default WhatWeOfferUi;
